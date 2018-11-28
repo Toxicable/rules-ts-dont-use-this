fa3d3092c468a08608023b33e3196e27baca6ac3
Fabian Wiles <fabian.wiles@gmail.com>
2018-11-29 10:31:34 +1300

    remove everything!

--- internal/tsc_wrapped/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './tsconfig';
-export * from './compiler_host';
-export * from './diagnostics';
-export * from './file_cache';
-export * from './worker';
-export * from './manifest';
-export * from './umd_module_declaration_transform';
